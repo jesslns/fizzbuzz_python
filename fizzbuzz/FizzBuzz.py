0b295cd19912a6569a11a8c35c09f2f219fe51ff
jesslns <jesslns@yahoo.com.hk>
2019-02-22 07:07:52 +0000

    Passed tests for divisibleByFive()

--- fizzbuzz/FizzBuzz.py
+++ fizzbuzz/FizzBuzz.py
@@ -4,4 +4,10 @@ def divisibleByThree(number):
     if number % 3 == 0:
         return "Fizz"
     else:
-        return 8
+        return number
+
+def divisibleByFive(number):
+    if number % 5 == 0:
+        return "Buzz"
+    else:
+        return number
--- fizzbuzz/test_FizzBuzz.py
+++ fizzbuzz/test_FizzBuzz.py
@@ -3,12 +3,19 @@ from fizzbuzz import FizzBuzz
 
 def test_divisibleByThree():
     assert FizzBuzz.divisibleByThree(6) == "Fizz"
+    assert FizzBuzz.divisibleByThree(21) == "Fizz"
+
 
 def test_notDivisibleByThree():
     assert FizzBuzz.divisibleByThree(8) == 8
+    assert FizzBuzz.divisibleByThree(14) == 14
 
 def test_divideByFive():
     assert FizzBuzz.divisibleByFive(20) == "Buzz"
+    assert FizzBuzz.divisibleByFive(25) == "Buzz"
 
 def test_notDivideByFive():
     assert FizzBuzz.divisibleByFive(13) == 13
+    assert FizzBuzz.divisibleByFive(11) == 11
+
+    
