6766e6333f9ade11577dc4e14f5c4378d0f338e9
jesslns <jesslns@yahoo.com.hk>
2019-02-22 07:12:28 +0000

    Wrote test for divideByThreeAndFive

--- fizzbuzz/test_FizzBuzz.py
+++ fizzbuzz/test_FizzBuzz.py
@@ -10,12 +10,18 @@ def test_notDivisibleByThree():
     assert FizzBuzz.divisibleByThree(8) == 8
     assert FizzBuzz.divisibleByThree(14) == 14
 
-def test_divideByFive():
+def test_divisibleByFive():
     assert FizzBuzz.divisibleByFive(20) == "Buzz"
     assert FizzBuzz.divisibleByFive(25) == "Buzz"
 
-def test_notDivideByFive():
+def test_notDivisibleByFive():
     assert FizzBuzz.divisibleByFive(13) == 13
     assert FizzBuzz.divisibleByFive(11) == 11
 
-    
+def test_divisibleByThreeAndFive():
+    assert FizzBuzz.divisibleByThreeAndFive(15) == "FizzBuzz"
+    assert FizzBuzz.divisibleByThreeAndFive(30) == "FizzBuzz"
+
+def test_notDivisibleByThreeAndFive():
+    assert FizzBuzz.divisibleByThreeAndFive(19) == 19
+    assert FizzBuzz.divisibleByThreeAndFive(7) == 7
