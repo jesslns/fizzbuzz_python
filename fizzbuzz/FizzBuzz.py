a4dbe772d8f557eff93fa49795677f3d9ef8dea8
jesslns <jesslns@yahoo.com.hk>
2019-02-22 07:30:11 +0000

    Passed fizzbuzz test: refactored three methods into one fizzbuzz method

--- fizzbuzz/FizzBuzz.py
+++ fizzbuzz/FizzBuzz.py
@@ -1,19 +1,29 @@
 import pytest
 
-def divisibleByThree(number):
-    if number % 3 == 0:
-        return "Fizz"
-    else:
-        return number
+# def divisibleByThree(number):
+#     if number % 3 == 0:
+#         return "Fizz"
+#     else:
+#         return number
+#
+# def divisibleByFive(number):
+#     if number % 5 == 0:
+#         return "Buzz"
+#     else:
+#         return number
+#
+# def divisibleByThreeAndFive(number):
+#     if number % 15 == 0:
+#         return "FizzBuzz"
+#     else:
+#         return number
 
-def divisibleByFive(number):
-    if number % 5 == 0:
-        return "Buzz"
-    else:
-        return number
-
-def divisibleByThreeAndFive(number):
+def fizzbuzz(number):
     if number % 15 == 0:
         return "FizzBuzz"
+    elif number % 3 == 0:
+        return "Fizz"
+    elif number % 5 == 0:
+        return "Buzz"
     else:
         return number
--- fizzbuzz/test_FizzBuzz.py
+++ fizzbuzz/test_FizzBuzz.py
@@ -5,26 +5,27 @@ def test_divisibleByThree():
     assert FizzBuzz.divisibleByThree(6) == "Fizz"
     assert FizzBuzz.divisibleByThree(21) == "Fizz"
 
-
-def test_notDivisibleByThree():
-    assert FizzBuzz.divisibleByThree(8) == 8
-    assert FizzBuzz.divisibleByThree(14) == 14
-
-def test_divisibleByFive():
-    assert FizzBuzz.divisibleByFive(20) == "Buzz"
-    assert FizzBuzz.divisibleByFive(25) == "Buzz"
-
-def test_notDivisibleByFive():
-    assert FizzBuzz.divisibleByFive(13) == 13
-    assert FizzBuzz.divisibleByFive(11) == 11
-
-def test_divisibleByThreeAndFive():
-    assert FizzBuzz.divisibleByThreeAndFive(15) == "FizzBuzz"
-    assert FizzBuzz.divisibleByThreeAndFive(30) == "FizzBuzz"
-
-def test_notDivisibleByThreeAndFive():
-    assert FizzBuzz.divisibleByThreeAndFive(19) == 19
-    assert FizzBuzz.divisibleByThreeAndFive(7) == 7
+# 
+# def test_notDivisibleByThree():
+#     assert FizzBuzz.divisibleByThree(8) == 8
+#     assert FizzBuzz.divisibleByThree(14) == 14
+#
+# def test_divisibleByFive():
+#     assert FizzBuzz.divisibleByFive(20) == "Buzz"
+#     assert FizzBuzz.divisibleByFive(25) == "Buzz"
+#
+# def test_notDivisibleByFive():
+#     assert FizzBuzz.divisibleByFive(13) == 13
+#     assert FizzBuzz.divisibleByFive(11) == 11
+#
+# def test_divisibleByThreeAndFive():
+#     assert FizzBuzz.divisibleByThreeAndFive(15) == "FizzBuzz"
+#     assert FizzBuzz.divisibleByThreeAndFive(30) == "FizzBuzz"
+#
+# def test_notDivisibleByThreeAndFive():
+#     assert FizzBuzz.divisibleByThreeAndFive(19) == 19
+#     assert FizzBuzz.divisibleByThreeAndFive(7) == 7
+#     assert FizzBuzz.divisibleByThreeAndFive(10) == 10
 
 def test_fizzbuzz():
     assert FizzBuzz.fizzbuzz(1) == 1
